f1d62cfa64965c07696e2f56f0b95e3bc1a04502
joeflow <thejoeflow@gmail.com>
2019-06-21 19:43:28 -0400

    Redirect all HTTP to HTTPS

--- src/main/kotlin/com/thejoeflow/config/SecurityConfig.kt
+++ src/main/kotlin/com/thejoeflow/config/SecurityConfig.kt
@@ -1,5 +1,9 @@
 package com.thejoeflow.config
 
+import org.apache.catalina.connector.Connector
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -24,8 +28,26 @@ class BCryptBean{
 @Configuration
 class WebSecurityConfiguration(private val customerUserDetailsService: CustomerUserDetailsService) : GlobalAuthenticationConfigurerAdapter() {
 
+    @Value("\${server.port.http}")
+    private val serverPortHttp: Int = 0
+
+    @Value("\${server.port}")
+    private val serverPortHttps: Int = 0
+
     override fun init(auth: AuthenticationManagerBuilder?) {
         auth!!.userDetailsService(customerUserDetailsService)
+    }
+
+    @Bean
+    fun servletContainer(): ServletWebServerFactory {
+        val tomcat = TomcatServletWebServerFactory()
+        val httpConnector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+        httpConnector.scheme = "http"
+        httpConnector.port = serverPortHttp
+        httpConnector.secure = false
+        httpConnector.redirectPort = serverPortHttps
+        tomcat.addAdditionalTomcatConnectors(httpConnector)
+        return tomcat
     }
 }
 
@@ -34,6 +56,7 @@ class WebSecurityConfiguration(private val customerUserDetailsService: CustomerU
 class SecurityJavaConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
+        http.requiresChannel().anyRequest().requiresSecure()
         http.csrf().disable()
         http.authorizeRequests().antMatchers("/api/*").fullyAuthenticated().and().httpBasic()
         http.authorizeRequests().antMatchers("/editPost/**").authenticated().and().formLogin()
